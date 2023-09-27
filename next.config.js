190a2edc7f3e4b3d64885d262fc045b9b395577c
Arun Bisht <aabisht@nagarro.com>
2023-09-27 16:09:18 +0530

    Localization added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const { i18n } = require("./next-i18next.config.js");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n,
+  reactStrictMode: false,
+};
 
 module.exports = nextConfig;
