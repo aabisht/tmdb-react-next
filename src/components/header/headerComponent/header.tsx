d57e7e5a73b3f21b2f8cc6f12f2306b883c42bb5
Arun Bisht <aabisht@nagarro.com>
2023-10-31 13:56:16 +0530

    Header baisc update

--- src/components/header/headerComponent/header.tsx
+++ src/components/header/headerComponent/header.tsx
@@ -2,22 +2,39 @@ import HeaderLogo from "../headerLogo/headerLogo";
 import { IIHeader } from "@type/headerTypes";
 import HeaderNavigation from "../headerNavigation/headerNavigation";
 import HeaderRightNavigation from "../headerRightNavigation/headerRightNavigation";
+import { useEffect, useState } from "react";
 
 const Header = ({ t, useDarkTheme = false }: IIHeader) => {
+  const [scrollTop, setScrollTop] = useState<number>(0);
+
+  useEffect(() => {
+    const onScroll = (event: any) => {
+      setScrollTop(event.target.documentElement.scrollTop);
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollTop]);
+
   return (
-    <header
-      className={`header-container-wrapper ${
-        useDarkTheme ? "dark-theme" : "light-theme"
-      }`}
-    >
-      <div className="header-wrapper pt-4 pb-4 relative after:content-[''] after:absolute after:z-0 after:w-[100%] after:h-[100%] after:top-[-35%] after:left-0 after:bg-gradient-to-b after:from-black/70 after:via-black/40 after:via-70% after:to-transparent">
+    <header className={`header-container-wrapper`}>
+      <div
+        className={`header-wrapper pt-4 pb-4 fixed left-0 right-0 w-[100%] transition-colors ease-in-out delay-150 z-10 after:content-[''] after:absolute after:z-0 after:w-[100%] after:h-[100%] after:top-[-35%] after:left-0 after:bg-gradient-to-b after:from-black/70 after:via-black/40 after:via-70% after:to-transparent ${
+          scrollTop > 63
+            ? useDarkTheme
+              ? "bg-black-light"
+              : "bg-primary"
+            : "bg-transparent"
+        }`}
+      >
         <div className="container xl:px-8 relative z-10">
-          <div className="flex flex-wrap justify-between content-center">
-            <div className="flex flex-wrap justify-start content-center">
-              <div className="content-center flex flex-wrap header-logo-wrapper lg:mr-8">
+          <div className="flex justify-between items-center">
+            <div className="flex justify-start items-center">
+              <div className="flex items-center header-logo-wrapper lg:mr-8">
                 <HeaderLogo t={t} />
               </div>
-              <div className="header-nav-wrapper flex flex-wrap content-center">
+              <div className="header-nav-wrapper flex items-center">
                 <HeaderNavigation t={t} />
               </div>
             </div>
